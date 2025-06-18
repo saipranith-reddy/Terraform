f999c92bf10d55dc7ca4108d9a04064ab4b48f6d
sai-pranith <saig.qawork@gmail.com>
2025-06-18 06:01:59 -0400

    changes to main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,14 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+  region = "us-east-1"  # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+  ami                    = "ami-09e6f87a47903347c"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-061374c5857ff8923"]
+  subnet_id              = "subnet-0797a64972604a5ac"
+
+  tags = {
+    Name        = "TerraformEC2"   
+  }
 }
